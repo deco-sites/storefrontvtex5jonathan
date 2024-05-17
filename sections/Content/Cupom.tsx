86b4012562c5babf08c968590bd2a460b4aa884e
JonathanMMSantos <jonathan.matheus.math9@outlook.com>
2024-05-17 01:30:47 -0300

    ajuste

---
+++ sections/Content/Cupom.tsx
@@ -0,0 +1,17 @@
+export interface Props {
+    codigo?: string,
+    description?: string,
+}
+
+
+
+export default function Cupom({ codigo, description }: Props){
+    return (
+        <div>
+            <span>
+                { codigo }
+                <p> { description } </p>
+            </span>
+        </div>
+    )
+}
